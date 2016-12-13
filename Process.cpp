8c8e8b5552b05fda35d6d8341bcb4928204e6ae1
tlblanton <tylerblanton635@gmail.com>
2016-12-13 10:52:41 -0700

    cleaned up code

--- Process.cpp
+++ Process.cpp
@@ -2,7 +2,7 @@
 //  Process.cpp
 //  OS-lab2-process-scheduling
 //
-//  Created by Tyler Blanton on 10/11/16.
+//  Created by Tyler Blanton on 10/12/16.
 //  Copyright © 2016 Tyler Blanton. All rights reserved.
 //
 
